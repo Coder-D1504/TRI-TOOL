f05dac238a3886dcff8cbb62be62a057e684ecb5
DIVYANSH SINGH RAWAT <142709754+Coder-D1504@users.noreply.github.com>
2025-12-24 14:00:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("qr-btn").onclick = function () {
+  window.location.href = "https://krish8555.github.io/TRItools/QR/qr.html"; // Link to QR Code Generator Page
+};
+
+document.getElementById("pwd-btn").onclick = function () {
+  window.location.href = "https://krish8555.github.io/TRItools/Pass/pass.html";
+};
+
+document.getElementById("todo-btn").onclick = function () {
+  window.location.href = "https://krish8555.github.io/TRItools/TODO/todo.html";
+};
+
